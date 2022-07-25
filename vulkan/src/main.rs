887d6b72725fce5216d62d6872c7b4f10a7ee225
Christian Swinehart <drafting@samizdat.co>
2022-07-25 11:26:52 -0400

    multithreaded vulkan windows

--- vulkan/src/main.rs
+++ vulkan/src/main.rs
@@ -1,220 +1,231 @@
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 #![allow(dead_code)]
-use env_logger::Env;
+#![allow(unused_imports)]
 
-use skulpin_renderer::ash_window;
-use skulpin_renderer::{ash, CoordinateSystem, CoordinateSystemHelper, RendererBuilder};
-use ash::vk;
-
-use skulpin_renderer::PhysicalSize as SRPhysicalSize;
-use skulpin_renderer::LogicalSize as SRLogicalSize;
-use skulpin_renderer::Window;
-use std::ffi::CStr;
-use ash::prelude::VkResult;
-use std::os::raw::c_char;
-
-#[derive(Clone)]
-pub struct WinitWindow<'a> {
-    window: &'a winit::window::Window,
-}
-
-impl<'a> WinitWindow<'a> {
-    pub fn new(window: &'a winit::window::Window) -> Self {
-        WinitWindow { window }
-    }
-}
-
-impl<'a> Window for WinitWindow<'a> {
-    fn physical_size(&self) -> SRPhysicalSize {
-        let physical_size: winit::dpi::PhysicalSize<u32> = self.window.inner_size();
-        SRPhysicalSize::new(physical_size.width, physical_size.height)
-    }
-
-    fn logical_size(&self) -> SRLogicalSize {
-        let logical_size: winit::dpi::LogicalSize<u32> = self
-            .window
-            .inner_size()
-            .to_logical(self.window.scale_factor());
-        SRLogicalSize::new(logical_size.width, logical_size.height)
-    }
-
-    fn scale_factor(&self) -> f64 {
-        self.window.scale_factor()
-    }
-
-    unsafe fn create_vulkan_surface(
-        &self,
-        entry: &ash::Entry,
-        instance: &ash::Instance,
-    ) -> VkResult<vk::SurfaceKHR> {
-        ash_window::create_surface(entry, instance, self.window, None)
-    }
-
-    fn extension_names(&self) -> VkResult<&'static [*const c_char]> {
-        ash_window::enumerate_required_extensions(&self.window)
-    }
-}
-
+use log;
+use std::sync::{Arc, Mutex};
+use std::collections::HashMap;
+use std::time::{Duration, Instant};
+use std::{sync::mpsc, thread};
+use skulpin::{CoordinateSystemHelper, CoordinateSystem, Renderer, RendererBuilder};
+use skulpin::rafx::api::RafxExtents2D;
+use skia_safe::{Point, Size, Rect, Color, Color4f, HSV, Paint};
 use winit::{
-    dpi::{LogicalPosition, LogicalSize, PhysicalSize},
-    event::{Event, WindowEvent},
+    dpi::{LogicalSize, LogicalPosition, PhysicalSize},
+    event::{Event, WindowEvent, KeyboardInput, VirtualKeyCode, ElementState},
     event_loop::{ControlFlow, EventLoop},
     platform::macos::WindowExtMacOS,
-    window::{WindowBuilder},
+    window::{WindowBuilder, Window},
 };
+
+struct VulkanWindow{
+    renderer: Arc<Mutex<Renderer>>,
+    window: Window,
+    color: HSV
+}
+
+unsafe impl Send for VulkanWindow {}
+
+impl VulkanWindow {
+    pub fn new(window:Window) -> Option<Self> {
+        let window_size = window.inner_size();
+        let window_extents = RafxExtents2D {
+            width: window_size.width,
+            height: window_size.height,
+        };
+
+        let renderer = RendererBuilder::new()
+            .coordinate_system(CoordinateSystem::Logical)
+            .build(&window, window_extents);
+
+        let color = HSV::from((0.5, 1.0, 0.3));
+
+        renderer.map(|renderer|
+            Self{ window, renderer:Arc::new(Mutex::new(renderer)), color }
+        ).ok()
+    }
+
+    pub fn resize(&mut self, _size: PhysicalSize<u32>) {
+        self.redraw().ok();
+    }
+
+    pub fn redraw(&mut self) -> Result<(), String>{
+        self.color.h += 1.0;
+        self.color.h %= 360.0;
+
+        let window_size = self.window.inner_size();
+        let window_extents = RafxExtents2D {
+            width: window_size.width,
+            height: window_size.height,
+        };
+
+        let mut s = 1.0/3.0 - 1.0/4.0 * ((self.color.h/180.0 * std::f32::consts::PI).cos() / 2.0 + 0.5);
+        let mut x = (self.color.h/180.0 * std::f32::consts::PI).sin() / 2.0 + 0.5;
+        let color:Color4f = self.color.to_color(255).into();
+
+        if let Err(e) = self.renderer.lock().unwrap().draw(
+            window_extents,
+            self.window.scale_factor(),
+            |canvas, coords| {
+                let cw = coords.window_logical_size().width as f32;
+                let ch = coords.window_logical_size().height as f32;
+                let w = 0.95 * cw;
+                let h = 0.95 * ch;
+
+                canvas.clear(Color4f::new(1.0, 1.0, 1.0, 1.0));
+
+                let rect = Rect::from_point_and_size(
+                    Point::new(
+                        (cw - w) / 2.0,
+                        (ch - h) / 2.0,
+                    ),
+                    (w,  h),
+                );
+                canvas.draw_rect(rect, &Paint::new(color, None));
+
+                x *= w;
+                s *= w;
+                let line = Rect::new(x-s/2.0, 0.0f32, x+s, ch);
+
+                let color:Color4f = Color::WHITE.into();
+                canvas.draw_rect(line, &Paint::new(color, None));
+            },
+        ){
+            Err(format!("Error in draw routine {}", e))
+        }else{
+            Ok(())
+        }
+    }
+}
 
 
 fn main() {
-    env_logger::Builder::from_env(Env::default().default_filter_or("debug")).init();
+    // Setup logging
+    env_logger::Builder::from_default_env()
+        .filter_level(log::LevelFilter::Debug)
+        .init();
 
     let event_loop = EventLoop::new();
 
+    const WINDOW_COUNT: usize = 4;
     let size:LogicalSize<i32> = LogicalSize::new(400, 300);
-    let loc:LogicalPosition<i32> = LogicalPosition::new(500, 300);
-    let logical_size = winit::dpi::LogicalSize::new(900.0, 600.0);
-    let visible_range = skia_safe::Rect {
-        left: 0.0,
-        right: logical_size.width as f32,
-        top: 0.0,
-        bottom: logical_size.height as f32,
-    };
-    let scale_to_fit = skia_safe::matrix::ScaleToFit::Center;
+    let mut loc:LogicalPosition<i32> = LogicalPosition::new(500, 300);
 
+    let mut window_senders = HashMap::with_capacity(WINDOW_COUNT);
+    for win_id in 0..WINDOW_COUNT {
 
-    let winit_window = WindowBuilder::new()
-        .with_inner_size(size)
-        .with_position(loc)
-        .with_title("Vulkan Window".to_string())
-        .build(&event_loop)
-        .unwrap();
+        let os_window = WindowBuilder::new()
+            .with_inner_size(size)
+            .with_position(loc)
+            .with_title("Vulkan Window".to_string())
+            .build(&event_loop)
+            .unwrap();
 
-    let window = WinitWindow::new(&winit_window);
+        loc.x += 30;
+        loc.y += 30;
+        let mut video_modes: Vec<_> = os_window.current_monitor().unwrap().video_modes().collect();
+        let mut video_mode_id = 0usize;
 
-    let renderer = RendererBuilder::new()
-        .use_vulkan_debug_layer(true)
-        .coordinate_system(CoordinateSystem::VisibleRange(
-            visible_range,
-            scale_to_fit,
-        ))
-        .build(&window);
+        let (tx, rx) = mpsc::channel();
+        window_senders.insert(os_window.id(), tx);
 
-    if let Err(e) = renderer {
-        println!("Error during renderer construction: {:?}", e);
-        return;
+        let mut window = VulkanWindow::new(os_window).unwrap();
+        window.color = match win_id {
+            0 => HSV::from((0.0, 1.0, 0.2)),
+            1 => HSV::from((90.0, 1.0, 0.5)),
+            2 => HSV::from((180.0, 1.0, 0.75)),
+            _ => HSV::from((270.0, 1.0, 1.0)),
+        };
+
+        thread::spawn(move || {
+            while let Ok(event) = rx.recv() {
+                match event {
+
+                    Event::WindowEvent { event, .. } => match event {
+                        WindowEvent::Moved { .. } => {
+                            // We need to update our chosen video mode if the window
+                            // was moved to an another monitor, so that the window
+                            // appears on this monitor instead when we go fullscreen
+                            let previous_video_mode = video_modes.get(video_mode_id).cloned();
+                            video_modes = window.window.current_monitor().unwrap().video_modes().collect();
+                            video_mode_id = video_mode_id.min(video_modes.len());
+                            let video_mode = video_modes.get(video_mode_id);
+
+                            // Different monitors may support different video modes,
+                            // and the index we chose previously may now point to a
+                            // completely different video mode, so notify the user
+                            if video_mode != previous_video_mode.as_ref() {
+                                println!(
+                                    "Window moved to another monitor, picked video mode: {}",
+                                    video_modes.get(video_mode_id).unwrap()
+                                );
+                            }
+                        },
+                        WindowEvent::Resized(size) => {
+                            window.resize(size);
+
+                        },
+                        _ => {}
+                    }
+                    Event::RedrawRequested(_) => {
+                        window.redraw().unwrap();
+                    },
+                    _ => {}
+                }
+            }
+        });
     }
 
-    let mut renderer = renderer.unwrap();
-
-    // Increment a frame count so we can render something that moves
-    let mut frame_count = 0;
+    let frame_time = Duration::from_micros(1_000_000 / 60);
+    let mut next_frame = Instant::now() + frame_time;
 
     // Start the window event loop. Winit will not return once run is called. We will get notified
     // when important events happen.
     event_loop.run(move |event, _window_target, control_flow| {
-        let window = WinitWindow::new(&winit_window);
-
         match event {
             //
             // Halt if the user requests to close the window
             //
-            winit::event::Event::WindowEvent {
-                event: winit::event::WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = winit::event_loop::ControlFlow::Exit,
+            Event::WindowEvent { event:ref win_event, window_id } => match win_event {
 
-            //
-            // Close if the escape key is hit
-            //
-            winit::event::Event::WindowEvent {
-                event:
-                    winit::event::WindowEvent::KeyboardInput {
-                        input:
-                            winit::event::KeyboardInput {
-                                virtual_keycode: Some(winit::event::VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    },
-                ..
-            } => *control_flow = winit::event_loop::ControlFlow::Exit,
-
-            //
-            // Request a redraw any time we finish processing events
-            //
-            winit::event::Event::MainEventsCleared => {
-                // Queue a RedrawRequested event.
-                winit_window.request_redraw();
-            }
-
-            //
-            // Redraw
-            //
-            winit::event::Event::RedrawRequested(_window_id) => {
-                if let Err(e) = renderer.draw(&window, |canvas, coordinate_system_helper| {
-                    draw(canvas, coordinate_system_helper, frame_count);
-                    frame_count += 1;
-                }) {
-                    println!("Error during draw: {:?}", e);
-                    *control_flow = winit::event_loop::ControlFlow::Exit
+                WindowEvent::KeyboardInput { input: KeyboardInput { state: ElementState::Released, virtual_keycode: Some(VirtualKeyCode::Escape), .. }, .. } |
+                WindowEvent::CloseRequested |
+                WindowEvent::Destroyed => {
+                    window_senders.remove(&window_id);
+                    if window_senders.is_empty(){
+                        *control_flow = ControlFlow::Exit;
+                    }
+                }
+                _ => {
+                    if let Some(tx) = window_senders.get(&window_id) {
+                        if let Some(event) = event.to_static() {
+                            tx.send(event).unwrap();
+                        }
+                    }
+                }
+            },
+            Event::RedrawRequested(window_id) => {
+                if let Some(tx) = window_senders.get(&window_id) {
+                    if let Some(event) = event.to_static() {
+                        tx.send(event).unwrap();
+                    }
                 }
             }
 
-            //
-            // Ignore all other events
-            //
+            Event::MainEventsCleared => {
+                // Queue a RedrawRequested event.
+                let now = Instant::now();
+                if now > next_frame{
+                    while next_frame < now {
+                        next_frame += frame_time;
+                    }
+                    for (win_id, tx) in window_senders.iter() {
+                        if let Some(event) = Event::RedrawRequested(*win_id).to_static() {
+                            tx.send(event).unwrap();
+                        }
+                    }
+                }
+            }
             _ => {}
         }
     });
-}
-
-/// Called when winit passes us a WindowEvent::RedrawRequested
-fn draw(
-    canvas: &mut skia_safe::Canvas,
-    _coordinate_system_helper: CoordinateSystemHelper,
-    frame_count: i32,
-) {
-    // Generally would want to clear data every time we draw
-    canvas.clear(skia_safe::Color::from_argb(0, 0, 0, 255));
-
-    // Floating point value constantly moving between 0..1 to generate some movement
-    let f = ((frame_count as f32 / 30.0).sin() + 1.0) / 2.0;
-
-    // Make a color to draw with
-    let mut paint = skia_safe::Paint::new(skia_safe::Color4f::new(1.0 - f, 0.0, f, 1.0), None);
-    paint.set_anti_alias(true);
-    paint.set_style(skia_safe::paint::Style::Stroke);
-    paint.set_stroke_width(2.0);
-
-    // Draw a line
-    canvas.draw_line(
-        skia_safe::Point::new(100.0, 500.0),
-        skia_safe::Point::new(800.0, 500.0),
-        &paint,
-    );
-
-    // Draw a circle
-    canvas.draw_circle(
-        skia_safe::Point::new(200.0 + (f * 500.0), 420.0),
-        50.0,
-        &paint,
-    );
-
-    // Draw a rectangle
-    canvas.draw_rect(
-        skia_safe::Rect {
-            left: 10.0,
-            top: 10.0,
-            right: 890.0,
-            bottom: 590.0,
-        },
-        &paint,
-    );
-
-    let mut font = skia_safe::Font::default();
-    font.set_size(100.0);
-
-    canvas.draw_str("Hello World", (65, 200), &font, &paint);
-    canvas.draw_str("Holo Whirled", (68, 203), &font, &paint);
-    canvas.draw_str("Hollow Whorled", (71, 206), &font, &paint);
 }
